1bcf828ed9ef2a2ba22f1ad80091a146e6852a39
KingScriptAE <king_lin2024@qq.com>
2024-06-21 10:06:29 +0800

    Update Je ne comprends rien.lua

--- Je ne comprends rien.lua
+++ Je ne comprends rien.lua
@@ -37,7 +37,7 @@ function Ripple(obj)
 		Ripple.BackgroundColor3 = Color3.fromRGB(139, 0, 255)
 		Ripple.BackgroundTransparency = 1.000
 		Ripple.ZIndex = 8
-		Ripple.Image = "rbxassetid://18139019694"
+		Ripple.Image = "rbxassetid://18139249668"
 		Ripple.ImageTransparency = 0.800
 		Ripple.ScaleType = Enum.ScaleType.Fit
 		Ripple.ImageColor3 = Color3.fromRGB(139, 0, 255)
@@ -224,7 +224,7 @@ end
       DropShadow.BackgroundTransparency = 1.000
       DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
       DropShadow.Size = UDim2.new(1, 10, 1, 10)
-      DropShadow.Image = "rbxassetid://18139019694" --背景
+      DropShadow.Image = "rbxassetid://18139249668" --背景
       DropShadow.ImageColor3 = Color3.fromRGB(255,255,255)
       DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
 
