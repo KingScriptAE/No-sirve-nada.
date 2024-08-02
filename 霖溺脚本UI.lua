03a15e47f9e3719d8ec0c038bed2cc62b65a66d3
KingScriptAE <king_lin2024@qq.com>
2024-08-02 22:34:10 +0800

    Add files via upload

--- 霖溺脚本UI.lua
+++ 霖溺脚本UI.lua
@@ -54,7 +54,7 @@ function Ripple(obj)
 		Ripple.BackgroundColor3 = Color3.fromRGB(139, 0, 255)
 		Ripple.BackgroundTransparency = 1.000
 		Ripple.ZIndex = 8
-		Ripple.Image = "rbxassetid://18745667640"
+		Ripple.Image = "rbxassetid://18766675197"
 		Ripple.ImageTransparency = 0.800
 		Ripple.ScaleType = Enum.ScaleType.Fit
 		Ripple.ImageColor3 = Color3.fromRGB(139, 0, 255)
@@ -242,7 +242,7 @@ end
       DropShadow.BackgroundTransparency = 1.000
       DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
       DropShadow.Size = UDim2.new(1, 10, 1, 10)
-      DropShadow.Image = "rbxassetid://18745667640" --背景
+      DropShadow.Image = "rbxassetid://18766675197" --背景
       DropShadow.ImageColor3 = Color3.fromRGB(255,255,255)
       DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
 
