a4482d9c68bc8368ea62eb23ba7576cf433fc136
KingScriptAE <king_lin2024@qq.com>
2025-06-27 13:10:51 +0800

    Add files via upload

---
+++ severlnscripuu.lua
@@ -0,0 +1,393 @@
+if "霖溺-免费版加载器" == LninniHubScript then
+-------------分割线------------------------分割线---------
+setclipboard("霖溺(感谢老铁支持)QQ新主群：http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=viOjjgj19-TUiZlamhpxb6uvWwVNGoB7&authKey=ACDi9sCtIis%2F4P%2BtirP046uWaJ54%2F149eBnUvaAsMu%2BWZwSFoEQrzZC9UDGFwmp%2F&noverify=0&group_code=744830231")
+
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ShenJiaoBen/ScriptLoader/refs/heads/main/2.lua"))()
+
+local marketplaceService = game:GetService("MarketplaceService")
+local cachedGameInfo = {}
+ 
+------------------====游戏ID列表====------------------
+local GameIDs = {
+    Ohio = {7239319209}, --俄亥俄州
+    ElementalPowerTycoon = {10253248401}, -- 元素力量大亨
+    EnergyAssault = {6172932937}, -- 能量突击
+    InfectiveSmile = {5985232436}, -- 感染的微笑
+    TheRake = {2413927524}, -- The Rake重制版
+    LegendOfPower = {3623096087}, -- 力量传奇
+    Planks = {140636953470579}, -- [TRADING]Planks 
+    BattlefieldTycoon = {31490825}, -- 战地大亨 
+    ColonialSurvival = {14888386963, 17362847861},-- 殖民地生存
+    NinjaLegend = {3956818381}, -- 忍者传奇
+    SpeedLegend = {3101667897}, --极速传奇
+    AttackingZombies = {1240123653}, -- 进击的僵尸 
+    HaikyuuLegends = {73956553001240}, -- 棒球传奇
+    FishTraining = {116432016116625}, -- 鱼的训练 
+    Forsaken = {18687417158}, --  被遗弃 
+    AllTheWayWest = {2474168535}, -- 一路向西 
+    BattleWarrior = {4282985734, 11979319180}, -- 战斗勇士 
+    GunFFA = {12137249458}, -- 枪战FFA
+    UltimateMiningTycoon = {18680867089}, -- 终极采矿大亨 
+    PlantingGarden = {126884695634066}, -- 种植花园
+    hideOrdie = {18799085098}, -- 隐藏或死亡
+    Bladeandbuffoonery = {117701570624742}, -- 刀片和buffoonery
+    nicooonextbots = {10118559731, 12629753855, 12135007762}, -- nico的下一个机器人
+    Evade = {9872472334, 10662542523, 10324347967, 10324346056, 10530697691, 10808838353}, -- Evade
+    PillarChase2 = {16560655288}, -- 柱子追逐2
+    MurderMystery2 = {893973440, 142823291},  -- 破坏者谜团2
+    DigToEarthCore = {81440632616906}, -- 挖掘地球核心
+    BubbleGumSimulatorINFINITY = {85896571713843}, -- 泡泡糖模拟器无限
+    Zombiegameupd3 = {14419907512}, -- 僵屍遊戲[更新3]
+    BreakIn2 = {13864667823, 13864661000, 14775231477}, -- Break In 2（故事）
+    RopeBattle = {136195938137126}, -- Rope Battle
+    UntitledDrillGame = {87700573492940}, --无标题钻机游戏
+    MergeBrainrot = {124387865885397} -- Merge Brainrot
+    
+}
+
+------------------====创作者ID列表====------------------
+local CreatorIDs = {
+    KillerAndSheriff = {32445464}, -- 杀手vs警长
+    BladeBall = {12836673}, -- 刀刃球
+    AnimeSaga = {16819392}, -- AnimeSaga[动漫传奇]
+    BloxFruits = {4372130}, -- BF水果
+    Skinwalkers = {35725548}, -- Skinwalkers剥皮者
+    BlueLockRivals = {34671275}, -- 蓝色锁:对手
+    PETAPETALevel6 = {32900163}, -- 无限旅馆
+    PixelBlade = {8605341},-- 像素之刀[抢先体验]
+    NoobsMustDie = {35729068}, -- 奴才必须死[Alpha]
+    BuildAnIsland = {33768122}, --建造一座岛屿
+    RIVALS = {3461453}, -- 竞争对手
+--  AnimeKingdomSimulator = {34106550}, -- 动漫王国模拟器
+    StealABrainrot = {35815907}, -- 偷走一粒脑红
+    Mines = {35786254}, -- 矿井
+    VoleyballLegends = {35330702}, -- 排球传奇
+    qidaylive = {33541525}, -- 七天生存
+    Lootify = {33799733}, -- 掠夺
+    Digthebackyard = {35630257}, -- 挖出后院
+    JiuJiuNightsintheforest = {6042520}, -- 森林中的99夜
+    AnimeRising = {34063840} -- 动漫崛起
+    
+}
+
+local function getCreatorId()
+    local placeId = game.PlaceId
+    if not cachedGameInfo[placeId] then
+        local success, result = pcall(function()
+            return marketplaceService:GetProductInfo(placeId).Creator.CreatorTargetId
+        end)
+        cachedGameInfo[placeId] = success and result or nil
+    end
+    return cachedGameInfo[placeId]
+end
+
+local function isInList(list)
+    for _, id in ipairs(list) do
+        if game.PlaceId == id then
+            return true
+        end
+    end
+    return false
+end
+
+local function isCreator(creatorList)
+    local creatorId = getCreatorId()
+    if not creatorId then return false end
+    
+    for _, id in ipairs(creatorList) do
+        if creatorId == id then
+            return true
+        end
+    end
+    return false
+end
+
+local function loadScript(url)
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+    if not success then
+        warn("脚本加载失败: " .. tostring(err))
+    end
+end
+
+-- game文字验证
+local function setGameVariables(gameName)
+    if gameName == "Ohio" then
+        KingScript = "By King"
+        Roblox = "草拟吗"
+        KingTeam = "King无敌"
+    elseif gameName == "ElementalPowerTycoon" then
+        LnScript = "霖溺-元素力量大亨"
+    elseif gameName == "EnergyAssault" then
+        LnScript = "霖溺-能量突击"
+    elseif gameName == "InfectiveSmile" then
+        LnScript = "霖溺-感染力微笑"
+    elseif gameName == "TheRake" then
+        LnScript = "霖溺-TheRake"
+    elseif gameName == "LegendOfPower" then
+        KingScript = "By LN"
+        Roblox = "霖溺Script"
+        KingTeam = "LegendofPower"
+    elseif gameName == "Planks" then
+        KingScript = "By King"
+        Roblox = "霖溺Script"
+        KingTeam = "霖溺Planks"
+    elseif gameName == "BattlefieldTycoon" then
+        KingScript = "By King"
+        Roblox = "霖溺"
+        KingTeam = "战地大亨"
+    elseif gameName == "ColonialSurvival" then
+        LnScript = "By Ln"
+        LnTeam = "免费"
+        ServerName = "殖民地生存"
+    elseif gameName == "NinjaLegend" then
+        KingScript = "By LN"
+        Roblox = "霖溺Script"
+        KingTeam = "LegendofNinja"
+    elseif gameName == "SpeedLegend" then
+        KingScript = "By LN"
+        Roblox = "霖溺Script"
+        KingTeam = "SpeedLegend"
+    elseif gameName == "AttackingZombies" then
+        LNScript = "By LN"
+        Roblox = "新年快乐"
+        LNTeam = "进击的僵尸"
+    elseif gameName == "HaikyuuLegends" then
+        KingScript = "By LN"
+        Roblox = "霖溺"
+        KingTeam = "Haikyuu Legends"
+    elseif gameName == "FishTraining" then
+        LNScript = "By LN"
+        Roblox = "鱼的训练"
+        LNTeam = "免费"
+    elseif gameName == "Forsaken" then
+        LnScript = "By Ln"
+        Roblox = "免费"
+        LNTeam = "被遗弃服务器"
+    elseif gameName == "AllTheWayWest" then
+        LnScript = "By Ln"
+        Roblox = "免费"
+        LNTeam = "一路向西"
+    elseif gameName == "BattleWarrior" then
+        LnScript = "霖溺-战斗勇士"
+    elseif gameName == "GunFFA" then
+        LnScript = "霖溺-枪战FFA"
+    elseif gameName == "UltimateMiningTycoon" then
+        LinniScript = "终极采矿大亨"
+        Roblox = "霖溺"
+    elseif gameName == "PlantingGarden" then
+        linniScript = "Linni"
+        Roblox = "种植花园"
+        LinniTeam = "挂机"
+    elseif gameName == "hideOrdie" then
+        LnScript = "霖溺-隐藏和死亡"
+    elseif gameName == "Bladeandbuffoonery" then
+        LnScript = "霖溺-刀片和buffoonery"
+    elseif gameName == "nicooonextbots" then
+        LnScript = "霖溺-nico下一个机器人"
+    elseif gameName == "Evade" then
+        LnScript = "霖溺-Evade"
+    elseif gameName == "PillarChase2" then
+        LnScript = "霖溺-柱子追逐2"
+    elseif gameName == "MurderMystery2" then
+        LnScript = "霖溺-破坏者谜团2"
+    elseif gameName == "DigToEarthCore" then
+        LnScript = "霖溺-挖掘地球核心"
+    elseif gameName == "BubbleGumSimulatorINFINITY" then
+        LnScript = "霖溺-泡泡糖模拟器无限"
+    elseif gameName == "Zombiegameupd3" then
+        LnScript = "霖溺-僵尸游戏[更新3]"
+    elseif gameName == "BreakIn2" then
+    LnScript = "霖溺-BreakIn2故事"
+        elseif gameName == "RopeBattle" then
+    LinniHubScript = "霖溺-RopeBattle"
+        elseif gameName == "UntitledDrillGame" then
+    LnScript = "霖溺-无标题钻机游戏"
+        elseif gameName == "MergeBrainrot" then
+    LinniHubScript = "霖溺-MergeBrainrot"
+
+    
+    
+    
+    
+    
+    
+    end
+end
+
+-- 创作者文字验证
+local function setCreatorVariables(creatorName)
+    if creatorName == "KillerAndSheriff" then
+        KingScript = "By King"
+        Roblox = "新飞月二飞春"
+        KingTeam = "霖溺杀手与警长"
+    elseif creatorName == "BladeBall" then
+        LnScript = "霖溺-刀刃球"
+    elseif creatorName == "AnimeSaga" then
+        LnScript = "霖溺-动漫传奇"
+    elseif creatorName == "BloxFruits" then
+        LnScript = "霖溺-BloxFruits"
+    elseif creatorName == "Skinwalkers" then
+        LnScript = "霖溺-Skinwalkers"
+    elseif creatorName == "BlueLockRivals" then
+        LnScript = "霖溺-蓝色锁:对手"
+    elseif creatorName == "PETAPETALevel6" then
+        LnScript = "霖溺-无限旅馆"
+    elseif creatorName == "PixelBlade" then
+        LnScript = "霖溺-像素之刀"
+    elseif creatorName == "NoobsMustDie" then
+        LnScript = "霖溺-奴才必须死"
+    elseif creatorName == "BuildAnIsland" then
+        LnScript = "霖溺-建造一座岛屿"
+    elseif creatorName == "RIVALS" then
+    LinniHubScript = "霖溺-RIVALS"
+--  elseif creatorName == "AnimeKingdomSimulator" then   
+    elseif creatorName == "StealABrainrot" then
+        LnScript = "霖溺-偷走一粒脑红"
+    elseif creatorName == "Mines" then
+        LinniHubScript = "霖溺-矿井"
+    elseif creatorName == "VoleyballLegends" then
+    LinniHubScript = "霖溺-排球传奇"
+    elseif creatorName == "qidaylive" then
+      LnScript = "霖溺-7天生存"
+    elseif creatorName == "Lootify" then
+        LnScript = "霖溺-掠夺"
+    elseif creatorName == "Digthebackyard" then
+        LnScript = "霖溺-挖出后院"
+    elseif creatorName == "JiuJiuNightsintheforest" then
+    LnScript = "霖溺-森林中的99夜"  
+         elseif creatorName == "AnimeRising" then 
+    LnScript = "霖溺-动漫崛起"
+     
+    
+        
+        
+    end
+end
+
+-- game脚本URL
+local function getGameScriptUrl(gameName)
+    local urls = {
+        Ohio = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/new_ohio.lua",
+        ElementalPowerTycoon = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_ElementalPowerTycoon.lua",
+        EnergyAssault = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_EnergyAssault.lua",
+        InfectiveSmile = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_InfectiveSmile.lua",
+        TheRake = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_TheRake.lua",
+        LegendOfPower = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LN%E5%8A%9B%E9%87%8F.lua",
+        Planks = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/Planks%EF%BC%88%E5%AD%90%E8%BF%BD%E5%92%8C%E8%87%AA%E7%9E%84%EF%BC%89%20.lua",
+        BattlefieldTycoon = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/%E6%88%98%E5%9C%B0%E5%A4%A7%E4%BA%A8%5B%E5%85%A8%E9%83%A8%E7%A7%92%E6%9D%80%5D.lua",
+        ColonialSurvival = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Colonial%20Survival%5Bhub%5D.lua",
+        NinjaLegend = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LN%E5%BF%8D%E8%80%85.lua",
+        SpeedLegend = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LN%E6%9E%81%E9%80%9F.lua",
+        AttackingZombies = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/%E8%BF%9B%E5%87%BB%E7%9A%84%E5%83%B5%E5%B0%B8.lua",
+        HaikyuuLegends = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Haikyuu%20Legends%20.lua",
+        FishTraining = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/%E9%B1%BC%E7%9A%84%E8%AE%AD%E7%BB%83.lua",
+        Forsaken = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Forsaken.lua",
+        AllTheWayWest = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Allthewaywest.lua",
+        BattleWarrior = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni-BattleWarrior.lua",
+        GunFFA = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni-GunFFA.lua",
+        UltimateMiningTycoon = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/%E9%9C%96%E6%BA%BA-%E7%BB%88%E6%9E%81%E9%87%87%E7%9F%BF%E5%A4%A7%E4%BA%A8.lua",
+        PlantingGarden = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/%E7%A7%8D%E6%A4%8D%E8%8A%B1%E5%9B%AD.lua",
+        hideOrdie = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni-Hide%20and%20die.lua",
+        Bladeandbuffoonery = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni-%E5%88%80%E7%89%87%E5%92%8Cbuffoonery.lua",
+        nicooonextbots = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_nicooonextbots.lua",
+        Evade = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_Evade.lua",
+        PillarChase2 = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_PillarChase2.lua",
+        MurderMystery2 = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_MurderMystery2.lua",
+        DigToEarthCore = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/DigToEarthCore.lua",
+        BubbleGumSimulatorINFINITY = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniHub_BubbleGumSimulatorINFINITY.lua",
+        Zombiegameupd3 = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Zombiegame%5Bupd3%5D.lua",
+        BreakIn2 = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniHubBreakIn2.lua",
+        RopeBattle = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniHub_RopeBattle.lua",
+        UntitledDrillGame = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniScriptHub_Untitled%20Drill%20Game.lua",
+        MergeBrainrot = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniScriptHub_Merge-Brainrot.lua"
+        
+        
+        
+        
+    }
+    return urls[gameName]
+end
+
+--创作者脚本url
+local function getCreatorScriptUrl(creatorName)
+    local urls = {
+        KillerAndSheriff = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/%E6%9D%80%E6%89%8B%E4%B8%8E%E8%AD%A6%E9%95%BF%EF%BC%88%E7%9B%AE%E5%89%8D%E6%AD%A3%E5%9C%A8FIX%EF%BC%89.lua",
+        BladeBall = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni-BladeBall.lua",
+        AnimeSaga = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_AnimeSaga.lua",
+        BloxFruits = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_BloxFruits.lua",
+        Skinwalkers = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/Linni_Skinwalkers.lua",
+        BlueLockRivals = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniHub_BlueLockRivals.lua",
+        PETAPETALevel6 = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/PETAPETALevel6.lua",
+        PixelBlade = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/PixelBlade%5BEarlyAccess%5D.lua",
+        NoobsMustDie = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/NoobsMustDie%5BAlpha%5D.lua",
+        BuildAnIsland = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/BuildAnIsland.lua",
+        RIVALS = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LINNISCRIPTHUB_RIVALS.lua",
+--        AnimeKingdomSimulator = "",
+        StealABrainrot = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniScriptHub-Steal-A-Brainrot.lua",
+        Mines = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniScriptHub_Mines.lua",
+        VoleyballLegends = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniHubScript_Voleyball-Legends.lua",
+        qidaylive = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniScript_7daylive.lua",
+        Lootify = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniScriptHub_Lootify.lua",
+        Digthebackyard = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniTeamRoblox_DigTheBackyard.lua",
+       JiuJiuNightsintheforest = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LinniTeam_99NightsInTheForest.lua",
+     AnimeRising = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/AnimeRisingLinniScriptHub.lua"
+
+
+
+
+
+
+
+        
+    }
+    return urls[creatorName]
+end
+
+local function detectAndLoad()
+
+    if Games.GamesById[game.PlaceId] then
+        loadScript(Games.GamesById[game.PlaceId].url)
+        return
+    end
+
+
+    for gameName, ids in pairs(GameIDs) do
+        if isInList(ids) then
+            setGameVariables(gameName)
+            local url = getGameScriptUrl(gameName)
+            if url then
+                loadScript(url)
+            else
+                warn("未找到" .. gameName .. "的脚本URL")
+            end
+            return
+        end
+    end
+
+
+    for creatorName, ids in pairs(CreatorIDs) do
+        if isCreator(ids) then
+            setCreatorVariables(creatorName)
+            local url = getCreatorScriptUrl(creatorName)
+            if url then
+                loadScript(url)
+            else
+                warn("未找到" .. creatorName .. "的脚本URL")
+            end
+            return
+        end
+    end
+
+    warn("未找到匹配的游戏id或创作者id脚本或者霖溺不支持该服务器")
+end
+
+
+detectAndLoad()
+
+wait(1)
+else
+setclipboard("霖溺QQ新主群：http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=viOjjgj19-TUiZlamhpxb6uvWwVNGoB7&authKey=ACDi9sCtIis%2F4P%2BtirP046uWaJ54%2F149eBnUvaAsMu%2BWZwSFoEQrzZC9UDGFwmp%2F&noverify=0&group_code=744830231")
+game.Players.LocalPlayer:Kick("你没进群/没复制全吧/可能更新了❌")
+end
